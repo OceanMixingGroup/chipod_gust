135f9339108832892bcdb76d517a472a24a7ff78
Johannes Becherer <johannes@coas.oregonstate.edu>
2017-02-07 11:39:54 -0800

    wrote shell script to update the software from github and to initialize the base directory

--- foldertree/unit/mfiles/init.sh
+++ foldertree/unit/mfiles/init.sh
@@ -1,1 +1,15 @@
-hallo
+#!/bin/bash
+#   created by: 
+#        Johannes Becherer
+#        Tue Feb  7 11:35:24 PST 2017
+#
+#        This shell script initalizes the mfile folder  of a chipod/gust directory by
+#           1) downloading the latest software versoin from github
+#           2) creating an out put directory
+#           3) copying the standard driver function in this directory 
+
+git clone https://github.com/jbecherer/chipod_gust
+
+mkdir out
+
+cp ./chipod_gust/driver/* ./
---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#   created by: 
+#        Johannes Becherer
+# Tue Feb  7 11:31:29 PST 2017
+# 
+# This shell script gets the lates version of chipod_gust software from github
+#
+
+git pull

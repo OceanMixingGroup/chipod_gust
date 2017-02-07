b9050a297d52504540cabdec8f268a255fff40bf
Johannes Becherer <johannes@coas.oregonstate.edu>
2017-02-07 12:04:40 -0800

    corrected update routine

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#
+#/bin/bash
 #   created by: 
 #        Johannes Becherer
 # Tue Feb  7 11:31:29 PST 2017
@@ -6,4 +7,4 @@
 # This shell script gets the lates version of chipod_gust software from github
 #
 
-git pull
+git pull origin master

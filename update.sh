03a1f1659e2856ad224b816500f25307a5952c71
Johannes Becherer <johannes@coas.oregonstate.edu>
2017-02-07 14:55:57 -0800

    the update script now also works on the matlab server

--- update.sh
+++ update.sh
@@ -7,4 +7,15 @@
 # This shell script gets the lates version of chipod_gust software from github
 #
 
-git pull origin master
+if [[ $(hostname) == matlab* ]]
+then
+      echo You are logged on to the matlab server! 
+      echo The github connection will be done via 128.193.69.189
+      ssh   mixing@128.193.69.189 'cd ~/ganges/work/chipod_gust; sh update.sh'
+
+      scp -r mixing@128.193.69.189:~/ganges/work/chipod_gust/* ./
+
+else
+      echo The chipod_gust software package is updated from github
+      git pull origin master
+fi

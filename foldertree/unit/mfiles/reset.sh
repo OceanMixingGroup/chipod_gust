8b95ce0c4f1340437da84a04cc39f52a4913105b
Johannes Becherer <johannes@coas.oregonstate.edu>
2017-02-07 11:51:34 -0800

    added shell script to set the mfile directory back to pre initialization

---
+++ foldertree/unit/mfiles/reset.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#   created by: 
+#        Johannes Becherer
+#        Tue Feb  7 11:35:24 PST 2017
+#
+#   This function will reset the mfile directory to its pre initialization stag
+
+
+echo Are you sure that you want to reset the mfile directory?
+echo this action will delete all driver routines from this directory
+echo type y/n
+read test
+
+if [ $test == 'y' ]
+then
+   echo !!!!!!!!!!!! GOOOOOO !!!!!!!!!
+      rm -rf ./chipod_gust
+      rm -r ./out
+
+      rm ./*.m
+      rm run.sh
+      rm kill_script.sh
+      rm monitorMatlab.sh
+else
+   echo action stoped
+fi
+
+
+

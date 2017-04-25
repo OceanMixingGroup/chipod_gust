df6e0c2ff5172582f6b37b28f86411333f5ecde5
deepak <dpak.cherian@gmail.com>
2017-04-24 17:43:53 -0700

    Add N2 and background speed based masking.
    1. Rework because stratification mask needs to only be calculated
       once. Unless mooring is not surface-pumped, should mask out based on
       internal dT/dz.
    
    2. Speed criterion is we throw out estimates if background speed < min_spd.
       Assuming that the backwash from instrument moving up/down is
       flushed away if background speed is > min_spd.
    
    3. Also mask based on instantaneous speed past sensor. If not large
       enough, spectrum isn't properly estimated.
    
    4. Also allow for possibility of turning off masking

--- driver/combine_turbulence.m
+++ driver/combine_turbulence.m
@@ -25,7 +25,6 @@ close all;
                   % sensed water volume
                   % 'm' for mooring, 'p' for pitot,
                   % '' to choose based on what was used in chi estimate
-
    avgwindow = 600; % averaging window in seconds
 
    % if you want to restrict the time range that should be combined use the following
@@ -92,30 +91,22 @@ if(do_combine)
          iiTrange = find( chi.time>= time_range(1) & chi.time<= time_range(2) );
 
          if do_mask
-             % only need to setup these masks once
-             if ~exist('const_mask', 'var')
-                 if mask_dTdz == 'i'
-                     % choose appropriate internal stratification for sensor
-                     Tz.Tz = Tz.(['Tz' ID(7)']);
-                 end
-
-                 Tzmask = interp1(Tz.time, Tz.Tz, chi.time(iiTrange));
-
-                 percent_mask_dTdz = sum(abs(Tzmask) < min_dTdz)/length(chi.time(iiTrange))*100;
-                 percent_mask_N2 = sum(chi.N2 < min_N2)/length(chi.time(iiTrange))*100;
-                 percent_mask_inst_spd = sum(chi.spd(iiTrange) < min_inst_spd)/length(chi.time(iiTrange))*100;
-                 disp([' dTdz will mask ' num2str(percent_mask_dTdz, '%.2f') ...
-                       ' % of estimates'])
-                 disp([' N2 will mask ' num2str(percent_mask_N2, '%.2f') ...
-                       ' % of estimates'])
-                 disp([' Inst speed will mask ' num2str(percent_mask_inst_spd, '%.2f') ...
-                       ' % of estimates'])
-
-
-                 const_mask = (abs(Tzmask) < min_dTdz) ...
-                     | (chi.N2 < min_N2) ...
-                     | (chi.spd < min_inst_spd);
+             if mask_dTdz == 'i'
+                 % choose appropriate internal stratification for sensor
+                 Tz.Tz = Tz.(['Tz' ID(7)']);
              end
+
+             Tzmask = interp1(Tz.time, Tz.Tz, chi.time(iiTrange));
+
+             percent_mask_dTdz = sum(abs(Tzmask) < min_dTdz)/length(chi.time(iiTrange))*100;
+             percent_mask_N2 = sum(chi.N2 < min_N2)/length(chi.time(iiTrange))*100;
+             percent_mask_inst_spd = sum(chi.spd(iiTrange) < min_inst_spd)/length(chi.time(iiTrange))*100;
+             disp([' dTdz will mask ' num2str(percent_mask_dTdz, '%.2f') ...
+                   ' % of estimates'])
+             disp([' N2 will mask ' num2str(percent_mask_N2, '%.2f') ...
+                   ' % of estimates'])
+             disp([' Inst speed will mask ' num2str(percent_mask_inst_spd, '%.2f') ...
+                   ' % of estimates'])
 
              % speed mask could change depending on estimate
              if strcmpi(mask_spd_initial, '')
@@ -136,7 +127,16 @@ if(do_combine)
              disp([' speed will mask ' num2str(percent_mask_spd, '%.2f') ...
                    '% of estimates'])
 
-             full_mask = const_mask | (spdmask < min_spd);
+             full_mask = (abs(Tzmask) < min_dTdz) ...
+                 | (chi.N2 < min_N2) ...
+                 | (chi.spd < min_inst_spd) ...
+                 | (spdmask < min_spd);
+         end
+
+         % NaN out some chi estimates based on min_dTz, min_spd
+         if do_mask
+             chi.chi(full_mask) = NaN;
+             chi.mask = chi.mask | ~full_mask;
          end
 
          % get list of all fields to average
@@ -146,12 +146,6 @@ if(do_combine)
             %% average data
             % convert averaging window from seconds to points
             ww =  round(avgwindow/(diff(chi.time(1:2))*3600*24));
-
-            % NaN out some chi estimates based on min_dTz, min_spd
-            if do_mask
-                chi.chi(full_mask) = NaN;
-                chi.mask = chi.mask | ~full_mask;
-            end
 
             for f = 1:length(ff)  % run through all fields in chi
                if ( length(chi.(ff{f})) == length(chi.time) )

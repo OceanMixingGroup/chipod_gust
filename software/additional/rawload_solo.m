1d8db75799601d928a3b3c5b8fe7ec5b431dff0b
jbecherer <jbecherer@gmx.net>
2017-07-08 00:21:14 +0000

    added routine to read SoloT raw-files

---
+++ software/additional/rawload_solo.m
@@ -0,0 +1,39 @@
+function [T] = rawload_solo(fid)
+%%  [T] = rawload_solo(fid)
+%
+%  This function reads in the raw files (sqlite, rsk)
+%  from SoloTs and returns a  mat struction structure 
+%
+%  INPUT
+%     fid      :  path to raw-file
+%
+%  OUTPUT
+%     T.time   :  matlab time
+%     T.T      :  temperature
+%
+%   created by: 
+%        Johannes Becherer
+%        Sat Jul  8 00:13:24 GMT 2017
+
+
+if nargin<1
+    [raw_name,rawdir]=uigetfile('*.*','Load Binary File');
+    fid=[temp raw_name];
+    if raw_name==0
+        error('File not found')
+        return
+    end
+end
+
+
+% open data base file
+db1 = sqlite(fid);
+
+% get data block
+data = fetch(db1,'select * from data');
+
+% temperature
+T.T    = double(cell2mat(data(:,2));
+
+% convert unix time from t logger into matlab time
+T.time = datenum(1970,1,1,0,0,double(cell2mat(data(:,1))/1000)));

cad528bc9a26b40b3e489b8e2bee588cd81b5b59
deepak <dpak.cherian@gmail.com>
2017-08-18 15:11:56 -0700

    Fix bad merge 3.

--- driver/combine_turbulence.m
+++ driver/combine_turbulence.m
@@ -381,10 +381,6 @@ if(do_combine)
 
 %_____________________save combined structure______________________
    save([savedir '/Turb.mat'], 'Turb');
-   if length(runname) ~= 0
-       system(['ln -s ' savedir '/Turb.mat ' savedir ...
-               '../combined/' runname '.mat']);
-   end
 end
 
 %_____________________comparison plot______________________

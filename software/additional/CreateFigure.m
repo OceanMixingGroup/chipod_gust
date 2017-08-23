465d8fa5963aefe2026bad08db520c258a076cb2
deepak <dpak.cherian@gmail.com>
2017-08-23 11:16:10 -0700

    opengl renderer by default.

--- software/additional/CreateFigure.m
+++ software/additional/CreateFigure.m
@@ -4,7 +4,7 @@ function [hfig] = CreateFigure()
                   'Position', [100 100 1400 900]);
 
     set(hfig, 'DefaultAxesFontSize', 18);
-    set(hfig, 'renderer', 'painters');
+    set(hfig, 'renderer', 'opengl');
 
     set(hfig,'DefaultAxesBox','on')
     set(hfig,'DefaultAxesTickDir','out')

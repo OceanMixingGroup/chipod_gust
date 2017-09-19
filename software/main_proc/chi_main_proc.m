7a833c69562e5d93fdf566c30451cab115b27966
Sally Warner <sally.warner@gmail.com>
2017-09-19 13:05:54 -0700

    added case to chi_main_proc.m allowing filenames that don't start with 'raw_'
    The software looks only for chipods with names that start with ‘raw_’.
    For older chipod datafiles, they often are just ‘YYMMDDHH.mat’. Added
    capability to read these files also

--- software/main_proc/chi_main_proc.m
+++ software/main_proc/chi_main_proc.m
@@ -27,6 +27,11 @@ function [] = chi_main_proc(basedir, rfid, pflag, varargin)
       savestamp   = [rfid((is1):(is2)) 'mat'];
       savedir     = [basedir filesep 'proc' filesep];
 
+      % in cases where no 'raw_' preceeds the date for the raw filenames,
+      % need different string for savestamp
+      if isempty(is1)
+         savestamp   = ['_' rfid(1:(is2)) 'mat'];
+      end
 
 %_____________________load raw_file______________________
    % load header

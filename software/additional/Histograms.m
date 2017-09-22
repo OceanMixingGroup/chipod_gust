17e52f65b83ddec5d4e2287a3c73f8a90b7d866b
deepak <dpak.cherian@gmail.com>
2017-09-22 10:47:31 -0700

    Histogram bins are now invariant. Should reduce confusion.

--- software/additional/Histograms.m
+++ software/additional/Histograms.m
@@ -1,5 +1,11 @@
 function Histograms(chi, hfig, normstr, ID)
 
+    nbins = 300;
+    chibins = linspace(-10, -3, nbins);
+    epsbins = linspace(-12, -2, nbins);
+    ktbins  = linspace(-8, 0, nbins);
+    jqbins  = linspace(-3, 4, nbins/2);
+
     figure(hfig)
     hfig.Position(3) = 2000;
     hfig.Position(4) = 1065;
@@ -7,7 +13,7 @@ function Histograms(chi, hfig, normstr, ID)
 
     hax(1) = subplot(221);
     set(gca, 'color', 'none')
-    myhist(chi.chi, normstr, ID)
+    myhist(chi.chi, chibins, normstr, ID)
     hold on;
     xlabel('log_{10} \chi')
     xlim([-12, -4])
@@ -15,7 +21,7 @@ function Histograms(chi, hfig, normstr, ID)
 
     hax(2) = subplot(222);
     set(gca, 'color', 'none')
-    myhist(chi.eps, normstr, ID)
+    myhist(chi.eps, epsbins, normstr, ID)
     xlim([-12, -2])
     xlabel('log_{10} \epsilon')
     ylabel(normstr)
@@ -23,7 +29,7 @@ function Histograms(chi, hfig, normstr, ID)
 
     hax(3) = subplot(223);
     set(gca, 'color', 'none')
-    myhist(chi.Kt, normstr, ID)
+    myhist(chi.Kt, ktbins, normstr, ID)
     xlim([-8, 2])
     hold on;
     xlabel('log_{10} K_T')
@@ -31,15 +37,14 @@ function Histograms(chi, hfig, normstr, ID)
 
     hax(4) = subplot(224);
     set(gca, 'color', 'none')
-    myhist(abs(chi.Jq), normstr, ID)
+    myhist(abs(chi.Jq), jqbins, normstr, ID)
     xlim([-5, 4.5])
     hold on;
     xlabel('log_{10} |J_q|')
     ylabel(normstr)
 end
 
-function myhist(var, normstr, ID)
-    nbins = ceil(sqrt(numel(var)));
+function myhist(var, bins, normstr, ID)
 
     avg = nanmean(var);
     med = nanmedian(var);
@@ -52,7 +57,7 @@ function myhist(var, normstr, ID)
     color = choose_color(ID,'color');
     lw = choose_color(ID,'width');
 
-    hh = histogram(log10(var), nbins, 'normalization', normstr, ...
+    hh = histogram(log10(var), 'BinEdges', bins, 'normalization', normstr, ...
                    'displayname', str, 'displaystyle', 'stairs', ...
                    'LineWidth', lw, 'EdgeColor', color);
 

2451048404e54588834b941fc50e7460d09415a6
deepak <dpak.cherian@gmail.com>
2018-01-10 18:56:08 -0800

    Updates to histograms.
    1. Update default histogram bins.
    2. better check for Tz masking
    3. Show averaged Kt, Jq in histograms instead of meaningless 1 sec estimates.

--- software/additional/Histograms.m
+++ software/additional/Histograms.m
@@ -1,10 +1,10 @@
 function Histograms(chi, hfig, normstr, ID, legendtext)
 
     nbins = 300;
-    chibins = linspace(-12, -3, nbins);
-    epsbins = linspace(-12, -2, nbins);
-    ktbins  = linspace(-8, 1, nbins);
-    jqbins  = linspace(-3, 4, nbins/2);
+    chibins = linspace(-12, -1, nbins);
+    epsbins = linspace(-12, 1, nbins);
+    ktbins  = linspace(-8, 3, nbins);
+    jqbins  = linspace(-3, 6, nbins/2);
 
     %figure(hfig)
     hfig.Position(3) = 2000;
@@ -16,31 +16,45 @@ function Histograms(chi, hfig, normstr, ID, legendtext)
     myhist(chi.chi, chibins, normstr, ID, legendtext)
     hold on;
     xlabel('log_{10} \chi')
-    xlim([-12, -4])
+    xlim([-12, -1])
     ylabel(normstr)
 
     hax(2) = subplot(222);
     set(gca, 'color', 'none')
     myhist(chi.eps, epsbins, normstr, ID, legendtext)
-    xlim([-12, -2])
+    xlim([-12, 1])
     xlabel('log_{10} \epsilon')
     ylabel(normstr)
     hold on;
 
+    if (chi.time(11) - chi.time(10))*86400 < 2
+        nt = 600;
+        % 1 sec data. average Kt, Jq
+        chival = moving_average(chi.chi, nt, nt);
+        dTdz = moving_average(chi.dTdz, nt, nt);
+        Kt = 0.5 * chival ./ dTdz;
+        Jq = -1025 .* 4200 .* Kt .* dTdz;
+        prefix = '10 min avg';
+    else
+        Kt = chi.Kt;
+        Jq = chi.Jq;
+        prefix = '';
+    end
+
     hax(3) = subplot(223);
     set(gca, 'color', 'none')
     myhist(chi.Kt, ktbins, normstr, ID, legendtext)
-    xlim([-8, 2])
+    xlim([-8, 3])
     hold on;
-    xlabel('log_{10} K_T')
+    xlabel([prefix 'log_{10} K_T'])
     ylabel(normstr)
 
     hax(4) = subplot(224);
     set(gca, 'color', 'none')
     myhist(abs(chi.Jq), jqbins, normstr, ID, legendtext)
-    xlim([-5, 4.5])
+    xlim([-5, 6])
     hold on;
-    xlabel('log_{10} |J_q|')
+    xlabel([prefix 'log_{10} |J_q|'])
     ylabel(normstr)
 end
 
@@ -57,7 +71,7 @@ function myhist(var, bins, normstr, ID, legendtext)
     if strcmp(legendtext,'raw')   
         color = [0 0 0];
         lw = 2;
-    elseif strcmp(legendtext(2:3),'Tz')   
+    elseif ~isempty(strfind(legendtext,'|Tz|'))
         color = [1 0 0];
         lw = 2;
     else

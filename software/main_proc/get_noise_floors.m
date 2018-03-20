fcfc2a4b8bb919cff3349eefb4915a6c83174c91
deepak <dpak.cherian@gmail.com>
2018-03-20 11:30:45 -0700

    Reword noise floor detection warnings

--- software/main_proc/get_noise_floors.m
+++ software/main_proc/get_noise_floors.m
@@ -34,9 +34,9 @@ function [Tp_floor, T_floor] = get_noise_floors(Tvolt, Tcoef, Tpcoef, Fs, ...
 
     dT = abs(diff(Tvec));
     dTmin = nanmin(dT(dT > 0));
-    if abs(T_floor - dTmin)/dTmin * 100 > 1
+    if abs(T_floor - dTmin)/dTmin * 100 > 5
         disp(['Warning: estimated temperature noise floor is different from ' ...
-              'min(abs(diff(T)) by more than 1% '])
+              'min(abs(diff(T)) by ' num2str(abs(T_floor - dTmin)/dTmin * 100) '%'])
     end
 
     % generate bit noise time series, and figure out variance when
@@ -54,7 +54,7 @@ function [Tp_floor, T_floor] = get_noise_floors(Tvolt, Tcoef, Tpcoef, Fs, ...
     % noise_floor * integral_(0)^(50 Hz) (df) = variance
     if abs(Tp_floor * Fs/2 - est_TP_floor)/est_TP_floor * 100 > 5
         disp(['Warning: estimated Tp noise floor is different from ' ...
-              'bit noise variance by more than 5% '])
+              'bit noise variance by ' num2str(abs(T_floor - dTmin)/dTmin * 100) '%'])
     end
 
     % noise floor debugging plots

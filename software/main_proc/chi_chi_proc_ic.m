5a492a8b12da51075d613d6407df3d451db36aa5
jbecherer <jbecherer@gmx.net>
2018-03-30 14:44:37 -0700

    now IC is based on temperature instead of Tp

--- software/main_proc/chi_chi_proc_ic.m
+++ software/main_proc/chi_chi_proc_ic.m
@@ -1,20 +1,20 @@
-function [chi] = chi_chi_proc_ic(Tp, S, Tz, T)
-%% [chi] = chi_chi_proc_ic(Tp, S, Tz)
+function [chi] = chi_chi_proc_ic(T, S, Tz, Tw, f_range)
+%% [chi] = chi_chi_proc_ic(T, S, Tz, [Tw], [f_range])
 %
 %   This is the main chi processing routine
 %
 %   INPUT
-%     Tp.time  :  time vector of temperature derivitve
-%     Tp.tp    :  temperature derivitve
+%     T.time   :  time vector of T
+%     T.T      :  T
+%     [T.S]    :  S (default set to 35 psu) 
+%     T.depth  :  depth
 %     S.time   :  time vector of spd 
 %     S.spd    :  speed
 %     Tz.time  :  time vector of Tz and N2   
 %     Tz.Tz    :  Tz
 %     Tz.N2    :  N2
-%     T.time   :  time vector of T
-%     T.T      :  T
-%     [T.S]    :  S (default set to 35 psu) 
-%     T.depth  :  depth
+%     Tw       :  length of spectral window (default 600sec)
+%     f_range  :  fitting range (default 1/50...1/20)
 %
 %   OUTPUT
 %     chi.chi  :  chi data
@@ -25,7 +25,7 @@ function [chi] = chi_chi_proc_ic(Tp, S, Tz, T)
 %     chi.S    :  salinity used to calculate viscosity
 %     chi.depth:  depth used to calculate viscosity
 %     chi.Tz   :  temperature gradietn used to do the calculations 
-%     chi.N2   :  N2 used to do the calculations 
+%     chi.N2   :  N2 used to do the calculations
 %     chi.mask :  mask used  
 %
 %   created by: 
@@ -34,79 +34,53 @@ function [chi] = chi_chi_proc_ic(Tp, S, Tz, T)
   
 
 %_____________________default values______________________
-
+if nargin < 4
+   Tw    = 600;            % length of the window in sec
+end
 if ~isfield(T, 'S') 
    T.S = ones(size([T.T]))*35;
 end
 
+% fitting range 
+if nargin < 5
+   f_range  =  [1/50 1/20];
+end
+if diff(f_range) < 0
+   f_range = f_range([2 1]);
+end
+fstart = f_range(1); fstop = f_range(2);  % integration range
 
-% define some constants
-fmax  = 15;
-gamma = 0.2;
-
-%disp('Using default transfer function: filter order 2, cutoff frequency 32')
-%_____________________spectral filter stuff______________________
-analog_filter_order          = 4;
-analog_filter_freq           = 50;
-trans_fcn                    = 0;
-trans_fcn1i                  = 0;
-trans_fcn2i                  = 0;
-thermistor_filter_order      = 2;
-thermistor_cutoff_frequency  = 32;
     
 
-%_____________________bring spd and Tp to same time grid______________________
-   if length(S.time) ~= length(Tp.time)
-      S.spd  = interp1( S.time, S.spd, Tp.time, 'nearest');
-      S.time = Tp.time;
+%_____________________bring spd and T to same time grid______________________
+   if length(S.time) ~= length(T.time)
+      S.spd  = interp1( S.time, S.spd, T.time, 'nearest');
+      S.time = T.time;
    end
 
 
 %_____________________cut time series into pieces for spectra______________________
-   Tw    = 600;            % length of the window in sec
-   dt    = diff(Tp.time(1:2))*3600*24;
+   dt    = nanmedian(diff(T.time))*3600*24;
    Nf    = round(Tw/(dt));   % Nf is the length of the Tw fragment
-   J{1}  = 1:length(Tp.time); 
+   Nf_2  = round(Nf/2);      % half a segment
+
+   ii_s  =  [(1+Nf_2):Nf:(length(T.time)-Nf_2)]; % index array for chi-subsample
+
+
+   J{1}  = 1:length(T.time); 
    I     = split_fragments(J, Nf, 0); % split in Tw intrevals with no overlap  
    Ni    = length(I);   % total number of Tw fragments  
 
 
 %_____________________construct final time vector______________________
-   chi.time = nan(1,Ni);
-   chi.spd  = nan(size(chi.time));
-   for i = 1:Ni
-      chi.time(i) = nanmean( Tp.time( I{i}) );
-       % average speed
-       chi.spd(i) = nanmean(S.spd(I{i}));
-   end
+   chi.time    = T.time(ii_s);
+   chi.spd     = clever_interp(S.time, S.spd, chi.time);
+   chi.dTdz    = clever_interp( Tz.time, Tz.Tz, chi.time);
+   chi.N2      = clever_interp( Tz.time, Tz.N2, chi.time);
+   chi.T       = clever_interp( T.time, T.T, chi.time);
+   chi.S       = clever_interp( T.time, T.S, chi.time);
+   chi.depth   = clever_interp( T.time, T.depth, chi.time);
 
-%_____________interp (average) stratification, temp, S, and depth on final time_______
-   
-   for i = 1:Ni
-      tl = chi.time(i)+ [-.5 .5]*Tw/(3600*24);     % define time interval  
-   
-      iiTz        = find( Tz.time>=tl(1) & Tz.time<=tl(2) ); % find all indexes that are within the time interval
-      if isempty(iiTz) % if there are no data points in the interval interp
-         chi.dTdz(i)    = interp1( Tz.time, Tz.Tz, chi.time(i));
-         chi.N2(i)      = interp1( Tz.time, Tz.N2, chi.time(i));
-      else % if there are datapoints in the interval average
-         chi.dTdz(i)    = nanmean( Tz.Tz(iiTz) );
-         chi.N2(i)      = nanmean( Tz.N2(iiTz) );
-      end
-
-      % same for T structure
-      iiT        = find( T.time>=tl(1) & T.time<=tl(2) ); % find all indexes that are within the time interval
-      if isempty(iiT) % if there are no data points in the interval interp
-         chi.T(i)    = interp1( T.time, T.T, chi.time(i));
-         chi.S(i)    = interp1( T.time, T.S, chi.time(i));
-         chi.depth(i)= interp1( T.time, T.depth, chi.time(i));
-      else % if there are datapoints in the interval average
-         chi.T(i)    = nanmean( T.T(iiT) );
-         chi.S(i)    = nanmean( T.S(iiT) );
-         chi.depth(i)= nanmean( T.depth(iiT) );
-      end
-
-   end
 
    %----------calculate viscosity and diffusivity-------------
       nu   = nan(Ni, 1);
@@ -117,7 +91,7 @@ thermistor_cutoff_frequency  = 32;
       end
    %---------------------spectral constants----------------------
          samplerate = Nf/Tw;
-         nfft       = Nf/2; % use two windows on entire time length
+         nfft       = floor(Nf/4)*2; % use two windows on entire time length making sure nfft is even
 
 %_____________________masking data______________________
    % avg.fspd(ik) >= 0.04  && dTdz(i)>min_dTdz 
@@ -139,26 +113,15 @@ thermistor_cutoff_frequency  = 32;
 
       if chi.mask(i)  % check if calculation should be executed 
 
-          % calculate psd of dT/dt 
-          [tp_power, freq] = fast_psd( Tp.tp(I{i}) ,nfft, samplerate);
+          [t_power, freq] = fast_psd( T.T(I{i}) ,nfft, samplerate);
 
-          % corrections with transfer functions
-          tp_power = invert_filt( freq, ...
-                                 invert_filt( freq, tp_power, thermistor_filter_order, thermistor_cutoff_frequency), ...
-                                 analog_filter_order, analog_filter_freq);
+          % calculate psd of dT/dt 
+          tp_power   = t_power.*(2*pi*freq).^2;
                                
 
-         % inertial range 
-            fstart = 1/200; fstop = 1/20;  % integration range
-
+          % fit the spectrum
          [chi_tmp, eps_tmp , k,spec, k_kraich, spec_kraich, stats] =...
                     get_chipod_chi_ic( freq,fstart,fstop, tp_power, chi.spd(i), nu(i), tdif(i), chi.dTdz(i), chi.N2(i));
-
-                    
-                          
-        %  % fit spectrum
-        %  [chi_tmp, eps_tmp , k,spec, k_kraich, spec_kraich, stats] =...
-        %          get_chipod_chi_new( freq,fstart,fstop, tp_power, chi.spd(i), nu(i), tdif(i), chi.dTdz(i), chi.N2(i)); 
 
 
           chi.chi(i) = chi_tmp(1);

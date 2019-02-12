5eaec16fa42c8e5e5b7397d40ce782a7b48a3e40
jbecherer <jbecherer@gmx.net>
2019-02-11 17:02:51 -0800

    added frequency axes to spec plot

--- driver/peps_spec_plot.m
+++ driver/peps_spec_plot.m
@@ -1,11 +1,11 @@
-%clear all;
+clear all;
 close all;
 
 addpath(genpath('./chipod_gust/software/'));
 
-do_load              = 0;
-plot_individual_spec = 0;
-plot_avg_spec        = 0;
+do_load              = 1;
+plot_individual_spec = 1;
+plot_avg_spec        = 1;
 
 if do_load 
    load ../proc/pitot_eps.mat;
@@ -13,6 +13,7 @@ end
 
 % special masks
 %Peps.eps = Peps.eps_nomask.*Peps.mask_flow.*((Peps.time>median(Peps.time(1))));
+Peps.eps = Peps.eps_nomask;
 
 
 nu = 1.6e-6;
@@ -34,7 +35,7 @@ eps(5) = 1e-5;
       k_vec = tmp(:);
       tmp = [Peps.D_k(:,ii_eps)];
       D_vec = tmp(:);
-      tmp = [Peps.D_k_acc(:,ii_eps)];
+      tmp = [Peps.D_k_acx(:,ii_eps)];
       D_vec_acc = tmp(:);
       for j = 1:length(k_grid)
          ii_kbin   =  find( k_vec>=(k_grid(j)-.5*dk_grid) & k_vec<=(k_grid(j)+.5*dk_grid)  );
@@ -82,6 +83,11 @@ if plot_avg_spec
          end
          ylabel(ax(a), 'D_{11}(k)/(15\nu) [s^{-2}(rad/m)^{-1}]');
          xlabel(ax(a), 'k [(rad/m)]');
+               [axf , ~] = copy_axes(ax);
+               xlf = xl*median(Peps.spd(~isnan(Peps.eps)))/(2*pi);
+               xlim(axf,xlf);
+               set(axf, 'XaxisLocation', 'top', 'Xscale', 'log', 'Xtick', [10.^(ceil(log10(xlf(1))):floor(log10(xlf(2))))]);
+               xlabel(axf, 'f [Hz]')
 
          set(ax(a), 'Yscale', 'log', 'Xscale', 'log');
             set(ax(a), 'box', 'on', 'TickDir', 'out');
@@ -91,14 +97,20 @@ end
 
 
 
-if plot_individual_specs
+if plot_individual_spec
     fig = figure('Color',[1 1 1],'visible','on','Paperunits','centimeters',...
             'Papersize',[30 20],'PaperPosition',[0 0 30 20])
     
 
+         col = get(groot,'DefaultAxesColorOrder');
 
          % another mask
-         ii_what = find(Peps.eps>1e-10);
+
+			varxy = log10((nanmean(Peps.D_k_acy(1:4,:))+nanmean(Peps.D_k_acx(1:4,:))) );
+
+         %ii_what = find(Peps.eps>1e-4&Peps.spd>.1);
+         ii_what = find(varxy>-4&Peps.spd>.1);
+         ii_what = find(Peps.eps<1e-5);
 
          for   s = 1:length(ii_what) 
 
@@ -106,21 +118,26 @@ if plot_individual_specs
                ax(a) = subplot( 1, 1, a);
                hold(ax(a), 'on');
             end
-            xl = [6e-1 1e3];
+            xl = [1e-2 1e5];
             xlim(ax(a), xl);
-            yl = [1e-6 3e-3];
+            yl = [1e-7 1e0];
             ylim(ax(a), yl);
-            plot(ax(a), Peps.k(:,ii_what(s)),movmean( Peps.D_k(:,ii_what(s)),5), 'Linewidth', 2);
+            plot(ax(a), Peps.k(:,ii_what(s)),movmean( Peps.D_k(:,ii_what(s)),1), 'Linewidth', 2);
+            plot(ax(a), Peps.k(:,ii_what(s)),movmean( Peps.D_k_org(:,ii_what(s)),1),'k', 'Linewidth', 2);
+            plot(ax(a), Peps.k(:,ii_what(s)),movmean( Peps.D_k_acx(:,ii_what(s)),5), 'Linewidth', 1);
+            plot(ax(a), Peps.k(:,ii_what(s)),movmean( Peps.D_k_acy(:,ii_what(s)),5), 'Linewidth', 1);
+            plot(ax(a), Peps.k(:,ii_what(s)),movmean( Peps.D_k_acz(:,ii_what(s)),5), 'Linewidth', 2);
             [D_na_now, k_na_now, ~] = nasmyth2Dk( kks, G1, Peps.eps(ii_what(s)), nu );
             plot(ax(a), k_na_now, D_na_now, '--', 'color', [0 0 0], 'Linewidth', 2);
             
 
             for i =1:length(eps)    
                plot(ax(a), k_na{i}, D_na{i}, '--', 'color',col(i,:)*.5, 'Linewidth', 1);
-               plot(ax(a), k_grid(ii_kmax), D_grid_acc(ii_kmax,i),'--','color',col(i,:), 'Linewidth', 1);
+              % plot(ax(a), k_grid(ii_kmax), D_grid_acc(ii_kmax,i),'--','color',col(i,:), 'Linewidth', 1);
                i_text = min(find(k_na{i}>xl(1)));
                t= text( xl(1)*1.3, D_na{i}(i_text), ['\epsilon = ' num2str(eps(i)) ' m^2/s^3']);
-               t.Color = col(i,:)*.8;
+               %t.Color = col(i,:)*.8;
+               t.Color = [.2 .2 .2];
                %t.FontWeight = 'bold';
                t.BackgroundColor = [1 1 1 .8];
                
@@ -128,6 +145,8 @@ if plot_individual_specs
             t = text_corner(ax(a), ['\epsilon = ' num2str(Peps.eps(ii_what(s))) ' ; speed = ' num2str(Peps.spd(ii_what(s)))], 1);
                t.BackgroundColor = [1 1 1];
                t.FontWeight      = 'bold';
+               legend(ax(a), 'Pitot corrected', 'Pitot uncoreccted', 'Ac_x', 'Ac_y', 'Ac_z');
+               
             
             
             
@@ -136,7 +155,14 @@ if plot_individual_specs
             xlabel(ax(a), 'k [(rad/m)]');
 
             set(ax(a), 'Yscale', 'log', 'Xscale', 'log');
-               set(ax(a), 'box', 'on', 'TickDir', 'out');
+              % set(ax(a), 'box', 'on', 'TickDir', 'out');
+               [axf , ~] = copy_axes(ax);
+               xlf = xl*Peps.spd(ii_what(s))/(2*pi)
+               xlim(axf,xlf);
+               set(axf, 'XaxisLocation', 'top', 'Xscale', 'log', 'Xtick', [10.^(ceil(log10(xlf(1))):floor(log10(xlf(2))))]);
+               xlabel(axf, 'f [Hz]')
+
+
                pause;
                clf;
 

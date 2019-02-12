cd13b76fdbe6605006f88e590c298597c8cc1799
jbecherer <jbecherer@gmx.net>
2019-02-11 17:03:46 -0800

    added emrging along time dimension if exist

--- software/additional/merge_cell_structs.m
+++ software/additional/merge_cell_structs.m
@@ -1,9 +1,9 @@
-% [merged] = merge_cell_structs(in, concat_2d)
+% [merged] = merge_cell_structs(in, concat_2d, concat_dim)
 %           in : cell array of structures
 %    concat_2d : if 0 (default), this routine combines 1d vectors to make a long
 %                1d vector
 %                if 1, this routine combines 1d vectors to yield a 2d array
-function [merged] = merge_cell_structs(in, concat_2d)
+function [merged] = merge_cell_structs(in, concat_2d, concat_dim)
 
     if ~exist('concat_2d', 'var'), concat_2d = 0; end
 
@@ -22,6 +22,14 @@ function [merged] = merge_cell_structs(in, concat_2d)
                     concat_dim = 1;
                 end
             end
+            if isfield(mat(1), 'time')
+                lentime = length(mat(1).time);
+                indtime = find(size(mat(1).(F{f})) == lentime);
+                if ~isempty(indtime)
+                    concat_dim = indtime;
+                end
+            end 
+
             merged.(F{f}) = cat(concat_dim, mat.(F{f}));
         end
     end

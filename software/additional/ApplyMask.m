dbe1ab1eea80de611d4d29ddfe91632646eeb425
dcherian <deepak@cherian.net>
2019-04-22 15:54:18 -0600

    Fastpath in ApplyMask: If mask wont do anything, skip

--- software/additional/ApplyMask.m
+++ software/additional/ApplyMask.m
@@ -20,26 +20,52 @@ function [chi, percentage, mask] = ApplyMask(chi, maskvar, relation, criterion, 
         mask = maskvar == criterion;
     end
 
-    % find number of NaNs prior to masking
-    if isnan(fill_value)
-        numnans = sum(isnan(chi.chi(Trange)));
+    if sum(mask) > 0 % fast path if mask is not going to do anything
+        % find number of NaNs prior to masking
+        if isnan(fill_value)
+            numnans = sum(isnan(chi.chi(Trange)));
+        else
+            numnans = sum(chi.chi(Trange) == fill_value);
+        end
+
+        % apply the mask
+        chi.chi(mask) = fill_value;
+        if fill_value == 0
+            warn_eps_nan = 1;
+            chi.eps(mask) = NaN;
+        else
+            warn_eps_nan = 0;
+            chi.eps(mask) = NaN;
+        end
+        chi.Kt(mask) = fill_value;
+        chi.Jq(mask) = fill_value;
+        chi.mask = isnan(chi.chi);
+
+        % count number of NaNs (or fill_value) after masking
+        if isnan(fill_value)
+            numnewnans = sum(isnan(chi.chi(Trange)));
+        else
+            numnewnans = sum(chi.chi(Trange) == fill_value);
+        end
+
+        % save percentage and print to screen.
+        dnans = (numnewnans-numnans);
+        percentage = dnans / length(Trange)*100;
+
+        if percentage > 0.5 & do_plot
+            perlabel = [label ' -' num2str(percentage, '%.1f') '%'];
+            Histograms(chi, hfig, 'count', ID, [perlabel]);
+        end
     else
-        numnans = sum(chi.chi(Trange) == fill_value);
+        percentage = 0;
+        dnans = 0;
+        warn_eps_nan = 0;
     end
 
-    % apply the mask
-    chi.chi(mask) = fill_value;
-    chi.eps(mask) = fill_value;
-    chi.Kt(mask) = fill_value;
-    chi.Jq(mask) = fill_value;
-    chi.mask = isnan(chi.chi);
-
-    % count number of NaNs (or fill_value) after masking
+    % get nice verb
     if isnan(fill_value)
-        numnewnans = sum(isnan(chi.chi(Trange)));
         verb = ' NaN-ed out ';
     else
-        numnewnans = sum(chi.chi(Trange) == fill_value);
         if fill_value == 0
             verb = ' zeroed out ';
         else
@@ -47,16 +73,11 @@ function [chi, percentage, mask] = ApplyMask(chi, maskvar, relation, criterion, 
         end
     end
 
-    % save percentage and print to screen.
-    dnans = (numnewnans-numnans);
-    percentage = dnans / length(Trange)*100;
     disp(['    ' maskname ' ' relation ' ' ...
           num2str(nanmedian(criterion), '%1.1e') ...
           verb num2str(percentage, '%.2f') ...
           '% of estimates (' num2str(dnans) ' points)'])
 
-    if percentage > 0.5 & do_plot
-        perlabel = [label ' -' num2str(percentage, '%.1f') '%'];
-        Histograms(chi, hfig, 'count', ID, [perlabel]);
-    end
+    if warn_eps_nan, disp('        Filling eps with NaN instead of 0.'); end
+
 end

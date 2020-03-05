27334826ccb1ab5a61a7667627997cf93dfbd8cd
Sally <sally.warner@gmail.com>
2020-03-05 14:48:05 -0800

    update default settings for calibrate_pitot.m
    The comment says, "averaging window for praw (default 600 [sec])," therefore the default should ACTUALLY BE 600 seconds not 30 seconds as it was set. I changed some other defaults settings as well.
    
    These are not things that should be changed in the master copy of the code for a specific deployment because there are many users who rely on this code.

--- driver/calibrate_pitot.m
+++ driver/calibrate_pitot.m
@@ -9,13 +9,13 @@ close all;
 
 
 %_____________________set processing flags______________________
-   do_parallel = 0;     % use paralelle computing 
-   do_raw_data = 0;     % do the averaging of the raw-data (1) or skip (0) if done before
-	avg_window = 30;  % averging window for praw (default 600 [sec])
-   do_v0_self  = 0;     % detremine V0 based on a min of the averaged signal (self contained)
-     DcalWindow =  1000;    % 1000 days window for V0 time (effectivly only a single value for the entire record)
-     DcalIncrement = 1000;  % 1000 day increment
-   do_v0_adcp  = 0;     % detremin V0 based on a fit against reference velocity (adcp) data
+   do_parallel = 1;     % use paralelle computing 
+   do_raw_data = 1;     % do the averaging of the raw-data (1) or skip (0) if done before
+   avg_window = 600;  % averging window for praw (default 600 [sec])
+   do_v0_self  = 1;     % detremine V0 based on a min of the averaged signal (self contained)
+   DcalWindow =  1000;    % 1000 days window for V0 time (effectivly only a single value for the entire record)
+   DcalIncrement = 1000;  % 1000 day increment
+   do_v0_adcp  = 1;     % detremin V0 based on a fit against reference velocity (adcp) data
    do_plot     = 0;     % generate some figures in ../pics/ to compare the different velocity estimates
    do_vel_p    = 0;     % which calibration should be used for vel_p (0 none (default), 1: adcp, 2: self)
    do_P1sec    = 0;     % this generates proc/P_1sec.mat based on data from temp.mat
